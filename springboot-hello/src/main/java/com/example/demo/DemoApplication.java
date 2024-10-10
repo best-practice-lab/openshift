6782d6ffc25313bdc1148a2a4c49d5bd37dbc866
Andy Wang <guohwang@redhat.com>
2024-10-10 09:46:17 +0800

    hello 2

--- springboot-hello/src/main/java/com/example/demo/DemoApplication.java
+++ springboot-hello/src/main/java/com/example/demo/DemoApplication.java
@@ -17,7 +17,7 @@ public class DemoApplication {
 
 	@GetMapping("/hello")
     public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
-      return String.format("Helloss ss asdfas sfasdf %s!", name);
+      return String.format("Hello 2 %s!", name);
     }
 
 }
